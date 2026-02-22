f182b6b3641a8af600e61530c7087624ac084954
junleerx <jun.lee3@baruchmail.cuny.edu>
2026-02-22 15:43:57 -0500

    build: install capacitor and add iOS platform configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.junleerx.workoutvibe',
+  appName: 'Workout Vibe Log',
+  webDir: 'dist'
+};
+
+export default config;
